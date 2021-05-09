6c59aa529e0c133eceb7500f1ecbdb4beae57c03
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-05-09 23:49:55 +0900

    fix top div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div >
+    <Box>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+    </Box>
   )
 }
