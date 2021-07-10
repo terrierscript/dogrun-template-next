66841759abf9f4c1c4153cb714ec3276630c9545
S. Suzuki <terrierscript@gmail.com>
2021-07-10 09:27:51 +0900

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiHandler } from "next"
 
-export default (req, res) => {
+const handler: NextApiHandler = (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
+export default handler
