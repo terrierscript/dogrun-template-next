94946fc2af4cc17349168bab240ec075d0f9df02
S. Suzuki <terrierscript@gmail.com>
2023-03-24 23:00:31 +0900

    rename

--- src/components/SampleComponent.tsx
+++ src/components/SampleComponent.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react"
 import { FC } from "react"
 
-export const Component: FC<{}> = () => {
+export const SampleComponent: FC<{}> = () => {
   return <Box></Box>
 }
