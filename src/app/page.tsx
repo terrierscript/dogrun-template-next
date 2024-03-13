75c431f61c0ad22217dbc4e0187e57bbd1490b82
S. Suzuki <terrierscript@gmail.com>
2024-03-13 21:23:02 +0900

    app router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ import { SampleComponent } from '../components/SampleComponent'
 export default function Home() {
   return (
     <Box>
-      <Head>
-        <title></title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Container>
         <SampleComponent />
       </Container>
