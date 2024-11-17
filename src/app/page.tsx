0077f8cbf6205ef8351e1d5bab2867cc3ff317be
S. Suzuki <terrierscript@gmail.com>
2024-11-17 23:10:33 +0900

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
 import { SampleComponent } from '../components/SampleComponent'
+import { Box, Container } from '@mantine/core'
 
 export default function Home() {
   return (
