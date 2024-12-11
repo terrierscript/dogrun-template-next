95da2b83e7e113d31543c72dfb941972473cecfa
S. Suzuki <terrierscript@gmail.com>
2024-12-11 11:49:48 +0900

    add mantineHtmlProps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ColorSchemeScript } from "@mantine/core"
+import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core"
 import { Provider } from "./AppProvider"
 import '@mantine/core/styles.css'
 
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
       </head>
